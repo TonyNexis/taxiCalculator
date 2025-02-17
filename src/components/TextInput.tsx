be38030f1cef3b2ecbbfc151dce7f30da147ce1f
Anton Tymchyk <tony.zenekis@gmail.com>
2025-02-17 23:32:03 +0200

    work on settingsPage form

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,41 +1,45 @@
 import InputAdornment from '@mui/material/InputAdornment'
 import { inputBaseClasses } from '@mui/material/InputBase'
 import TextField from '@mui/material/TextField'
+import { forwardRef } from 'react'
 
 interface TextInputProps {
-    id: string;
-    label: string;
-    unitType: string;
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  }
-
-const TextInput: React.FC<TextInputProps> = ({id, label, unitType, onChange}) => {
- return (
-    <TextField
-    id={id}
-    label={label}
-    onChange={onChange}
-    variant='outlined'
-    slotProps={{
-        input: {
-            endAdornment: (
-                <InputAdornment
-                    position='end'
-                    sx={{
-                        opacity: 0,
-                        pointerEvents: 'none',
-                        [`[data-shrink=true] ~ .${inputBaseClasses.root} > &`]: {
-                            opacity: 1,
-                        },
-                    }}
-                >
-                    {unitType}
-                </InputAdornment>
-            ),
-        },
-    }}
-/>
- )
+	id: string
+	label: string
+	unitType: string
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
+
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
+	({ id, label, unitType, onChange }, ref) => {
+		return (
+			<TextField
+				id={id}
+				label={label}
+				onChange={onChange}
+				variant='outlined'
+				inputRef={ref} 
+				slotProps={{
+					input: {
+						endAdornment: (
+							<InputAdornment
+								position='end'
+								sx={{
+									opacity: 0,
+									pointerEvents: 'none',
+									[`[data-shrink=true] ~ .${inputBaseClasses.root} > &`]: {
+										opacity: 1,
+									},
+								}}
+							>
+								{unitType}
+							</InputAdornment>
+						),
+					},
+				}}
+			/>
+		)
+	}
+)
 
 export default TextInput
--- src/pages/SettingsInfo/SettingsPage.tsx
+++ src/pages/SettingsInfo/SettingsPage.tsx
@@ -1,15 +1,45 @@
-import Button from '@mui/material/Button';
-import styles from './SettingsPage.module.scss'
+import Button from '@mui/material/Button'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import TextInput from '../../components/TextInput'
+import styles from './SettingsPage.module.scss'
+
+type Inputs = {
+	fuelPrice: number
+	depreciation: number
+}
 
 const SettingsPage = () => {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Inputs>()
+
+	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
 	return (
-		<div className={styles.settingsPage}>
-			<p className={styles.infoText}>Збережіть вартість пального та бажану суму амортизації </p>
-			<TextInput id = 'fuel-price' label = 'Вартість пального' unitType = 'грн/л'/>
-			<TextInput id = 'depreciation' label = 'Амортизація' unitType = 'грн/км'/>
-			<Button variant="contained">Зберегти</Button>
-		</div>
+		<form onSubmit={handleSubmit(onSubmit)} className={styles.settingsPage}>
+			<p className={styles.infoText}>
+				Збережіть вартість пального та бажану суму амортизації{' '}
+			</p>
+			<TextInput
+				{...register('fuelPrice', { required: true })}
+				id='fuel-price'
+				label='Вартість пального'
+				unitType='грн/л'
+			/>
+			<TextInput
+				{...register('depreciation')}
+				id='depreciation'
+				label='Амортизація'
+				unitType='грн/км'
+			/>
+			{errors.fuelPrice && <span>This field is required</span>}
+			<Button type='submit' variant='contained'>
+				Зберегти
+			</Button>
+		</form>
 	)
 }
 
